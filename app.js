904fa6a057f7809f3bf5c4dee96ccc86715ac3aa
TheodoreDev <theodore.delbove@gmail.com>
2023-10-01 01:14:36 +0200

    - Log in sys - Register sys - Log out sys - sessions sys

--- app.js
+++ app.js
@@ -2,15 +2,37 @@ const express = require("express");
 const app = express();
 const sqlite3 = require("sqlite3");
 const bcrypt = require("bcrypt");
+const passport = require("passport");
+const flash = require("express-flash");
+const session = require("express-session");
+const methodOverride = require('method-override')
+
+const config = require("./config.json");
+
+const initializePassport = require('./passport-config')
+initializePassport(
+  passport,
+  username => users.find(user => user.username === username),
+  id => users.find(user => user.id === id)
+)
+
+const users = []
 
 app.use(express.static("views/html"));
 app.use(express.static("views/steelsheet"))
 app.use(express.static("views/js"))
 
 app.set('view-engine', 'ejs')
-app.use(express.urlencoded({extented : false}))
-
-const config = require("./config.json");
+app.use(express.urlencoded({ extended: false }))
+app.use(flash())
+app.use(session({
+    secret: config.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(methodOverride('_method'))
 
 let db = new sqlite3.Database(`./Ressources/DB/${config.dbNAME}.db`, err => {
     if(err){
@@ -20,30 +42,59 @@ let db = new sqlite3.Database(`./Ressources/DB/${config.dbNAME}.db`, err => {
     }
 })
 
-const users = []
-
-app.get('/', (req, res) => {
+app.get('/', checkNotAuthenticated, (req, res) => {
     res.render("./html/index.ejs")
 })
 
-app.post('/login', (req, res) => {
+app.get('/home', checkAuthenticated, (req, res) => {
+    res.render("./html/home.ejs", {name: req.user.username})
+})
+
+app.post('/login', checkNotAuthenticated, passport.authenticate("local", {
+    successRedirect: '/home',
+    failureRedirect: '/',
+    failureFlash: true
+}))
+
+app.post('/register', checkNotAuthenticated, async (req, res) => {
     try {
-        db.get('SELECT * FROM user', (err, data) => {
-            if(err){
-                throw err
-            } else {
-                console.log(data)
-            }
+        const hashedPassword = await bcrypt.hash(req.body.password, 10)
+        users.push({
+          id: Date.now().toString(),
+          username: req.body.username,
+          email: req.body.email,
+          password: hashedPassword
         })
-    } catch (error) {
-        throw error
+        res.redirect('/')
+    } catch {
+        res.redirect('/')
     }
 })
 
-app.post('/register', async (req, res) => {
-    
+app.delete('/logout', (req, res, next) => {
+    req.logOut((err) => {
+        if (err) {
+            return next(err)
+        }
+        res.redirect('/')
+    })
 })
+
+function checkAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+      return next()
+    }
+  
+    res.redirect('/')
+}
+
+function checkNotAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+      return res.redirect('/home')
+    }
+    next()
+}  
 
 app.listen(config.devPort, function() {
     console.log(`Listen on port ${config.devPort}`)
-});
+})
