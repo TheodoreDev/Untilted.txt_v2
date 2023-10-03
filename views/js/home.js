6142fbb51864e630350359a9c3e1bf666eb1721d
TheodoreDev <theodore.delbove@gmail.com>
2023-10-04 00:38:37 +0200

    - Finish ioverlay and pop up for disconnect - Add return home button in preferences

--- views/js/home.js
+++ views/js/home.js
@@ -10,9 +10,20 @@ for (var i = 0; i < arrow.length; i++) {
 
 let disconnect = document.querySelectorAll(".button_disconnect")
 let overlay = document.querySelector(".overlay")
+let deco_popup = document.querySelector(".deco_popup")
 
 for(var i = 0; i < disconnect.length; i++) {
     disconnect[i].addEventListener("click", () => {
         overlay.classList.add("active")
+        deco_popup.classList.add("active")
+    })
+}
+
+let close_popup = document.querySelectorAll(".close_popup")
+
+for(var i = 0; i < close_popup.length; i++) {
+    close_popup[i].addEventListener("click", () => {
+        overlay.classList.remove("active")
+        deco_popup.classList.remove("active")
     })
 }
