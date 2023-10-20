1918c6d250a0e7806dd212bd02ac9f994e0b4cf3
TheodoreDev <theodore.delbove@gmail.com>
2023-10-21 00:29:10 +0200

    - analyctics circle in relation with percentage - fix some bugs

--- views/js/dashboard.js
+++ views/js/dashboard.js
@@ -8,27 +8,24 @@ if (theme_indicator == "1") {
     document.body.classList.add("light-theme")
 }
 
-const sideMenu = document.querySelector('aside');
-const menuBtn = document.getElementById('menu-btn');
-const closeBtn = document.getElementById('close-btn');
-
-menuBtn.addEventListener('click', () => {
-    sideMenu.style.display = 'block';
-});
-
-closeBtn.addEventListener('click', () => {
-    sideMenu.style.display = 'none';
-});
-
 Orders.forEach(order => {
     const tr = document.createElement('tr');
     const trContent = `
-        <td>${order.productName}</td>
-        <td>${order.productNumber}</td>
-        <td>${order.paymentStatus}</td>
+        <td>${order.uploadName}</td>
+        <td>${order.uploadNumber}</td>
+        <td>${order.User}</td>
         <td class="${order.status === 'Declined' ? 'danger' : order.status === 'Pending' ? 'warning' : 'primary'}">${order.status}</td>
         <td class="primary">Details</td>
     `;
     tr.innerHTML = trContent;
     document.querySelector('table tbody').appendChild(tr);
 });
+
+const percentage_analyctics = document.querySelectorAll(".percentage")
+
+for(var i = 0; i < percentage_analyctics.length; i++) {
+    const analyctic = percentage_analyctics[i].parentElement
+    const circle = analyctic.querySelector('circle')
+    const percentage = parseInt(percentage_analyctics[i].textContent)
+    circle.style.strokeDashoffset = `${226 - percentage/100*226}`
+}
--- views/js/order.js
+++ views/js/order.js
@@ -1,20 +1,20 @@
 const Orders = [
     {
-        productName: 'JavaScript Tutorial',
-        productNumber: '85743',
-        paymentStatus: 'Due',
-        status: 'Pending'
+        uploadName: 'Node js simple app',
+        uploadNumber: '85743',
+        User: 'Ted',
+        status: 'Active'
     },
     {
-        productName: 'CSS Full Course',
-        productNumber: '97245',
-        paymentStatus: 'Refunded',
+        uploadName: 'Snake in python',
+        uploadNumber: '97245',
+        User: 'Maxime',
         status: 'Declined'
     },
     {
-        productName: 'Flex-Box Tutorial',
-        productNumber: '36452',
-        paymentStatus: 'Paid',
-        status: 'Active'
+        uploadName: 'Mod Minecraft project',
+        uploadNumber: '36452',
+        User: 'LargonGaming',
+        status: 'Pending'
     },
 ]
